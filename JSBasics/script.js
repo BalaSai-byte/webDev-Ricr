97f15d18f07daaeeabea0aa84862af0611cb021c
BalaSai-byte <mabalasai2005@gmail.com>
2026-04-20 23:19:22 +0530

    operators and variables in java script

---
+++ JSBasics/script.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+
+// variable
+let x = 30; //method1
+var y = 40; //method2
+let z='30';//method3 for string
+
+//for making any variable constant
+constb=35;
+
+//operators
+console.log(x==z);//equal equal to operator
+
+console.log(x===z);
+
+console.log(typeof x);
+console.log(typeof z);
+
+
+
